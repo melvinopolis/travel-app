ebcdc5cce651135ab3f7657b5406da398910312d
Mariel Van Norman <melvinopolis@gmail.com>
2015-03-11 08:54:41 -0400

    adds google maps to index and show pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,51 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+
+	function initialize() {
+		var myLatlng = new google.maps.LatLng(35.2270941, -80.8462741);
+
+		var mapOptions = {
+			center: myLatlng,
+			zoom: 2,
+			scrollwheel: false
+		};
+
+		// var locations = [];
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			animation: google.maps.Animation.DROP
+		});
+
+		// for (i = 0; i < destination.last.id; i++) {
+		// 	locations = [destination.latitude, destination.longitude].push
+		// 		alert(locations);
+		// 	for (n = 0; n < locations.length; n++) {
+		// 		var locationMarker = new google.maps.Marker({
+		// 		position: locations[i],
+		// 		map: map,
+		// 		animation: google.maps.Animation.DROP
+		// 		});
+		// 	}
+		// }
+
+		var contentString = 'You are here!';
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+});
